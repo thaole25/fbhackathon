dd6a65fe18d1a3e80b78811983cf4837f30be7e4
thaole <thao.t.le25@gmail.com>
2019-05-04 16:33:56 +1000

    delete views in urls pages

--- educo/pages/urls.py
+++ educo/pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView, TeacherView, StudentView
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
