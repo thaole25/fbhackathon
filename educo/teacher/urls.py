d884fdbbea820942547468c98f74b6cfa9036f12
zintrulcre <zintrulcre@gmail.com>
2019-05-04 23:51:40 +1000

    Add Preview Page

--- educo/teacher/urls.py
+++ educo/teacher/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import TeacherView, MaterialMarketView
+from .views import *
 
 urlpatterns = [
     path('teacherhome', TeacherView.as_view(), name='studenthome'),
     path('materialmarket', MaterialMarketView.as_view(), name='materialmarket'),
+    path('materialcontent', MaterialContentView.as_view(), name='materialcontent')
 ]
