0446ef08e66f03ab3faa26ca124ce3e80ccb6e60
FreddieMe <chen3645935@hotmail.com>
2019-05-04 23:31:51 +1000

    update student dash

--- educo/student/urls.py
+++ educo/student/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import StudentView
+from .views import StudentView, StudentDashView
 
 urlpatterns = [
     path('', StudentView.as_view(), name='student'),
+    path('dash', StudentDashView.as_view(), name='studentdash'),
 ]
--- educo/teacher/urls.py
+++ educo/teacher/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import TeacherView, MaterialMarketView
+from .views import TeacherView, MaterialMarketView, TeacherDashView
 
 urlpatterns = [
     path('teacherhome', TeacherView.as_view(), name='studenthome'),
     path('materialmarket', MaterialMarketView.as_view(), name='materialmarket'),
+    path('dash', TeacherDashView.as_view(), name='teacherdash'),
 ]
