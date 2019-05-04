8ae1d5f8c26a1967f027541d7956962090e19d94
Dovermore <calvinyeye0627@gmail.com>
2019-05-04 22:36:12 +1000

    Fixed a problem wrt teacher url

--- educo/teacher/urls.py
+++ educo/teacher/urls.py
@@ -3,10 +3,6 @@ from django.urls import path
 from .views import TeacherView, MaterialMarketView
 
 urlpatterns = [
-<<<<<<< HEAD
     path('teacherhome', TeacherView.as_view(), name='studenthome'),
     path('materialmarket', MaterialMarketView.as_view(), name='materialmarket'),
-=======
-    path('teacherhome', TeacherView.as_view(), name='teacherhome'),
->>>>>>> 06c30511b8925fbb42852acb01b997682972a646
 ]
