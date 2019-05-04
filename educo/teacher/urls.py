76840a6be1fb326695c32980f2bda5e1e02489c9
zintrulcre <zintrulcre@gmail.com>
2019-05-04 23:58:07 +1000

    bug

--- educo/teacher/urls.py
+++ educo/teacher/urls.py
@@ -5,6 +5,6 @@ from .views import *
 urlpatterns = [
     path('teacherhome', TeacherView.as_view(), name='studenthome'),
     path('materialmarket', MaterialMarketView.as_view(), name='materialmarket'),
-    path('materialcontent', MaterialContentView.as_view(), name='materialcontent')
+    path('materialcontent', MaterialContentView.as_view(), name='materialcontent'),
     path('dash', TeacherDashView.as_view(), name='teacherdash'),
 ]
